9919101058bf57c324eb36fd36b5d6326c5fb827
Dennis Nissle <info@vendidero.de>
2021-09-16 11:49:23 +0200

    Adding /vendor directory to release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'cdfdc8aafb9f24803c3741ce79d572266715f4fc',
+    'name' => 'vendidero/one-stop-shop-woocommerce',
+  ),
+  'versions' => 
+  array (
+    'automattic/jetpack-autoloader' => 
+    array (
+      'pretty_version' => 'v2.10.5',
+      'version' => '2.10.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ec66177eefa1126d5319f3135ff3a7185e21a0ba',
+    ),
+    'vendidero/one-stop-shop-woocommerce' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cdfdc8aafb9f24803c3741ce79d572266715f4fc',
+    ),
+  ),
+);
