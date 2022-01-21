3e58f1f39d3190fed2a1297be2245c359dd12f92
vendidero <info@vendidero.de>
2022-01-21 15:11:01 +0100

    Adding /vendor directory to release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd233bfef1e038a4de00231cebe6787e3852856b5',
+        'name' => 'vendidero/one-stop-shop-woocommerce',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'automattic/jetpack-autoloader' => array(
+            'pretty_version' => 'v2.10.11',
+            'version' => '2.10.11.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
+            'aliases' => array(),
+            'reference' => '924226c0a9e2f9b0be022fc6bab2a90f5e610ef3',
+            'dev_requirement' => false,
+        ),
+        'vendidero/one-stop-shop-woocommerce' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd233bfef1e038a4de00231cebe6787e3852856b5',
+            'dev_requirement' => false,
+        ),
+    ),
+);
