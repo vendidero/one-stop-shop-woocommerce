ed4503f41b0494bde021a18fb01c740b77f03be4
vendidero <info@vendidero.de>
2022-11-01 18:19:37 +0100

    Adding /vendor directory to release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,62 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.3.2',
+        'version' => '1.3.2.0',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'vendidero/one-stop-shop-woocommerce',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'automattic/jetpack-autoloader' => array(
+            'pretty_version' => 'v2.11.11',
+            'version' => '2.11.11.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../automattic/jetpack-autoloader',
+            'aliases' => array(),
+            'reference' => 'c4c5e729bf797b67518936f28053ad9bd396562b',
+            'dev_requirement' => false,
+        ),
+        'composer/installers' => array(
+            'pretty_version' => 'v1.12.0',
+            'version' => '1.12.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
+            'dev_requirement' => false,
+        ),
+        'roundcube/plugin-installer' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'shama/baton' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'vendidero/one-stop-shop-woocommerce' => array(
+            'pretty_version' => '1.3.2',
+            'version' => '1.3.2.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'vendidero/woocommerce-eu-tax-helper' => array(
+            'pretty_version' => 'v1.0.4',
+            'version' => '1.0.4.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../libs/woocommerce-eu-tax-helper',
+            'aliases' => array(),
+            'reference' => '7748aaed604ef337e81a55ce4f08e970c3088c4d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
